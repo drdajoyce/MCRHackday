02c82048f10f779d010f94c197974d2bdfb8055f
Duncan Joyce <Duncan_Joyce@waters.com>
2020-08-17 11:56:07 +0100

    Revert to MVC model

--- HackDay/HackDay/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ HackDay/HackDay/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,18 +6,11 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public forecasts: any;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+    http.get<any>("https://data.police.uk/api/crimes-at-location?date=2020-02&lat=52.629729&lng=-1.131592").subscribe(result => {
+      this.forecasts = result[0][0];
     }, error => console.error(error));
   }
-}
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
 }
---
+++ HackDayApp/HackDayApp/Controllers/CrimeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace HackDayApp.Controllers
+{
+    public class CrimeController :Controller
+    {
+        public IActionResult GetCrimes()
+        {
+        List<Crime> model = null;
+        var client = new HttpClient();
+        var task = client.GetAsync("https://data.police.uk/api/crimes-at-location?date=2020-03&lat=52.629729&lng=-1.131592")
+          .ContinueWith((taskwithresponse) =>
+          {
+              var response = taskwithresponse.Result;
+              var jsonString = response.Content.ReadAsStringAsync();
+              jsonString.Wait();
+              model = JsonSerializer.Deserialize<List<Crime>>(jsonString.Result);
+          });
+        task.Wait();
+
+            return View(model);
+        }
+
+    }
+}
