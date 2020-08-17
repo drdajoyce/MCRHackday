ff9a41b8f3cb4ecec12751028b6f4b24386c2746
robyoung91 <robertmatthewyoung@gmail.com>
2020-08-17 12:38:14 +0100

    Add user form for lat/long input

---
+++ HackDayApp/HackDayApp/Models/UserLocation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackDayApp.Models
+{
+    public class UserLocation
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
